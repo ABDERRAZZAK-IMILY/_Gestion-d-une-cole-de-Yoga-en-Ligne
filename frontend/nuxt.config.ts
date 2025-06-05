43e791a02862f4777332b18a6587cf434ff7e244
abderrazzak imily <imily2024@gmail.com>
2025-06-05 19:19:30 +0100

    install vuetify

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  runtimeConfig: {
+    public: {
+      apiBase: 'http://localhost:8000/api' 
+    }
+  },
+
+  css: [
+    'vuetify/styles'
+  ],
+
+  build: {
+    transpile: ['vuetify']
+  },
+
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    }
+  }
 })
